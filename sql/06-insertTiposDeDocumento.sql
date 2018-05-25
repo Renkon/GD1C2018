41573a732a367bc3bbdf36a511a3cec6d718478c
Fernando (Renkon) <fmbesteiro@gmail.com>
2018-05-25 02:20:25 -0300

    agrego tipos doc

---
+++ sql/06-insertTiposDeDocumento.sql
@@ -0,0 +1,7 @@
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[tipos_documento] VALUES ('Documento nacional de identidad', 'DNI');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[tipos_documento] VALUES ('Libreta de enrolamiento', 'LE');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[tipos_documento] VALUES ('Libreta cívica', 'LC');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[tipos_documento] VALUES ('Cédula de identidad del Mercosur', 'CM');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[tipos_documento] VALUES ('Pasaporte', 'PP');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[tipos_documento] VALUES ('Desconocido', 'DD');
+
