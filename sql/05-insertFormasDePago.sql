064ed719a78979f9263e8aed7551f195c7c7a9ea
Fernando (Renkon) <fmbesteiro@gmail.com>
2018-05-25 01:46:12 -0300

    Agrego formas de pago

---
+++ sql/05-insertFormasDePago.sql
@@ -0,0 +1,13 @@
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[formas_de_pago] VALUES ('Efectivo');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[formas_de_pago] VALUES ('Tarjeta de crédito');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[formas_de_pago] VALUES ('Tarjeta de débito');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[formas_de_pago] VALUES ('Transferencia bancaria');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[formas_de_pago] VALUES ('Cheque al portador');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[formas_de_pago] VALUES ('Cheque a la orden');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[formas_de_pago] VALUES ('Cheque cancelatorio');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[formas_de_pago] VALUES ('PayPal');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[formas_de_pago] VALUES ('Bitcoin');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[formas_de_pago] VALUES ('MercadoPago');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[formas_de_pago] VALUES ('Rapipago');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[formas_de_pago] VALUES ('Cupón de premio');
+
