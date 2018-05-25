d90b7c0d23a85fb8aa9f4359b5046310b52e5a73
Fernando (Renkon) <fmbesteiro@gmail.com>
2018-05-25 01:22:17 -0300

    Agrego estados reserva

---
+++ sql/04-insertEstadosReserva.sql
@@ -0,0 +1,8 @@
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[estados_reserva] VALUES ('Reserva correcta');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[estados_reserva] VALUES ('Reserva modificada');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[estados_reserva] VALUES ('Reserva cancelada por recepción');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[estados_reserva] VALUES ('Reserva cancelada por cliente');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[estados_reserva] VALUES ('Reserva cancelada por no-show');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[estados_reserva] VALUES ('Reserva con ingreso (efectivizada)');
+INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[estados_reserva] VALUES ('Reserva desconocida');
+
