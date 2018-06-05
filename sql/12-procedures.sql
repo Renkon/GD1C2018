e060e0ea32c8bfbdf781ce15050d05abd8a68b92
Fernando (Renkon) <fmbesteiro@gmail.com>
2018-06-05 20:12:17 -0300

    Agrego 2 procedures

--- sql/12-procedures.sql
+++ sql/12-procedures.sql
@@ -6,3 +6,14 @@ FROM [EL_MONSTRUO_DEL_LAGO_MASER].[funcionalidades]
 
 GO
 
+-- Obtiene el ID de un usuario dummy (para los visitantes)
+CREATE PROCEDURE [EL_MONSTRUO_DEL_LAGO_MASER].[OBTENER_USUARIO_DUMMY]
+AS
+	SELECT 1 id_usuario
+GO
+
+-- Obtiene el ID del rol guest (que es 1 por nuestra lógicaa)
+CREATE PROCEDURE [EL_MONSTRUO_DEL_LAGO_MASER].[OBTENER_ROL_GUEST]
+AS
+	SELECT 1 id_rol
+GO
