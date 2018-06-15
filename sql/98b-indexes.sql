2df81769a143efeefa57084a29d715adc74f6ee6
Fernando (Renkon) <fmbesteiro@gmail.com>
2018-06-14 21:55:27 -0300

    agrego indices de clientes

---
+++ sql/98b-indexes.sql
@@ -0,0 +1,5 @@
+CREATE INDEX index_cliente_nombre ON [EL_MONSTRUO_DEL_LAGO_MASER].[clientes] (nombre_cliente)
+CREATE INDEX index_cliente_apellido ON [EL_MONSTRUO_DEL_LAGO_MASER].[clientes] (apellido_cliente)
+CREATE INDEX index_cliente_correo ON [EL_MONSTRUO_DEL_LAGO_MASER].[clientes] (correo_cliente)
+
+GO
