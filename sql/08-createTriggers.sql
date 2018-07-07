40e82e426bcc8bc7e542c22a2817f4b7b30e8fe4
Fernando (Renkon) <fmbesteiro@gmail.com>
2018-07-07 00:55:35 -0300

    Fix trigger

--- sql/08-createTriggers.sql
+++ sql/08-createTriggers.sql
@@ -1,14 +1,13 @@
 CREATE TRIGGER [EL_MONSTRUO_DEL_LAGO_MASER].[add_users_for_new_hotel] ON [EL_MONSTRUO_DEL_LAGO_MASER].[hoteles]
 AFTER INSERT AS
 BEGIN
-        DECLARE @id_hotel INT;
-        SELECT @id_hotel = id_hotel FROM inserted;
+    INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[usuariosXhoteles]
+    SELECT 1, id_hotel
+    FROM inserted
 
-        INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[usuariosXhoteles]
-        VALUES (1, @id_hotel);
-
-        INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[usuariosXhoteles]
-        VALUES (2, @id_hotel);
+    INSERT INTO [EL_MONSTRUO_DEL_LAGO_MASER].[usuariosXhoteles]
+    SELECT 2, id_hotel
+    FROM inserted
 END;
 
 GO
